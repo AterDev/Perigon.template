1c45c1114bf585b3bbd74cb51887945115970b7a
niltor <zpty@outlook.com>
2025-12-04 11:47:13 +0800

    移除迁移文件及事务指南，调整项目配置
    删除数据库迁移文件和事务使用指南，简化相关逻辑。
    更新 Pack.csproj 排除规则，移除 UserMod 和 SystemMod。
    新增对 CMSMod 的支持，优化安装脚本流程。

--- install.ps1
+++ install.ps1
@@ -1,14 +1,10 @@
 $OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
 
 $location = Get-Location
-$templatePath = Join-Path $location "../src/Template/"
-$solutionPath = Join-Path $location "../src/Template/templates/ApiStandard"
+$solutionPath = Join-Path $location "ApiStandard"
 $modulePath = Join-Path $solutionPath "src/Modules"
-$packPath = Join-Path $templatePath "nupkg"
+$packPath = Join-Path $location "nupkg"
 $requiredModules = @("SystemMod", "CommonMod");
-
-$removeModules = Get-ChildItem -Path $modulePath -Directory | Where-Object { $_.Name -notin $requiredModules }
-
 
 Write-Host "Clean files"
 # delete nupkg files
@@ -16,30 +12,12 @@ if (Test-Path $packPath) {
     Remove-Item $packPath -Force -Recurse
 }
 # delete files
-$migrationPath = Join-Path $solutionPath "../src/Template/templates/ApiStandard/src/Definition/EntityFramework/Migrations"
+$migrationPath = Join-Path $solutionPath "src/Definition/EntityFramework/Migrations"
 if (Test-Path $migrationPath) {
     Remove-Item $migrationPath -Force -Recurse
 }
 
-# backup slnx file
-$slnFile = Join-Path $solutionPath "MyProjectName.slnx"
-$backupFile = Join-Path $env:TEMP "MyProjectName.slnx.bak"
-if (Test-Path $slnFile) {
-    if (Test-Path $backupFile) {
-        Remove-Item $backupFile -Force
-    }
-    Copy-Item -Path $slnFile -Destination $backupFile
-}
-
 try {
-    Set-Location $solutionPath
-    # remove modules from solution
-    $removeModules | ForEach-Object { dotnet sln $slnFile remove  "$($_.FullName)\$($_.Name).csproj" }
-
-    # Set-Location $location
-    # exit;
-
-    Set-Location $templatePath
     # pack
     dotnet pack -c release -o $packPath
     # get package info
@@ -52,12 +30,6 @@ try {
     dotnet new uninstall $PackageId
     dotnet new install $packPath\$PackageId.$Version.nupkg
 
-    # restore slnx file
-    if (Test-Path $backupFile) {
-        Remove-Item $slnFile -Force
-        Copy-Item -Path $backupFile -Destination $slnFile
-    }
-    Remove-Item $backupFile -Force
     Set-Location $location;
 }
 catch {

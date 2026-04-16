ad19c1f72026e30c7ac1430a2872baa6d5ec09a4
niltor <zpty@outlook.com>
2026-04-16 15:59:28 +0800

    update: upgrade package versions in Directory.Packages.props, AppHost.csproj, and Perigon.AspNetCore.Toolkit.csproj; refactor ExcelService to use MiniExcelLibs

--- ApiStandard/src/Perigon/Perigon.AspNetCore.Toolkit/Services/ExcelService.cs
+++ ApiStandard/src/Perigon/Perigon.AspNetCore.Toolkit/Services/ExcelService.cs
@@ -1,5 +1,5 @@
-using OfficeOpenXml;
-using OfficeOpenXml.Export.ToCollection;
+using MiniExcelLibs;
+using MiniExcelLibs.OpenXml;
 
 namespace Perigon.AspNetCore.Toolkit.Services;
 
@@ -28,20 +28,20 @@ public class ExcelService
     )
     {
         var stream = new MemoryStream();
-        using (var package = new ExcelPackage(stream))
+        var configuration = new OpenXmlConfiguration
         {
-            ExcelWorksheet sheet = package.Workbook.Worksheets.Add(sheetName);
-            var list = data.ToList();
-            var excelRange = sheet.Cells[1, 1].LoadFromCollection(list, hasTitle);
+            FastMode = true,
+            EnableAutoWidth = true,
+            AutoFilter = false,
+            FreezeRowCount = hasTitle ? 1 : 0,
+        };
 
-            if (hasTitle)
-            {
-                sheet.Cells[1, 1, 1, excelRange.Columns].Style.Font.Bold = true;
-            }
-
-            sheet.Cells[sheet.Dimension.Address].AutoFitColumns();
-            await package.SaveAsync();
-        }
+        await stream.SaveAsAsync(
+            data,
+            printHeader: hasTitle,
+            sheetName: sheetName,
+            configuration: configuration
+        );
         stream.Position = 0;
         return stream;
     }
@@ -55,28 +55,9 @@ public class ExcelService
     /// <param name="hasTitle">是否包含标题</param>
     /// <returns></returns>
     public static List<T> Import<T>(Stream stream, string? sheetName = null, bool hasTitle = true)
+        where T : class, new()
     {
-        var data = new List<T>();
-        using var package = new ExcelPackage(stream);
-        ExcelWorksheet sheet =
-            sheetName == null
-                ? package.Workbook.Worksheets[0]
-                : package.Workbook.Worksheets[sheetName];
-
-        var rows = sheet.Dimension.Rows;
-        var columns = sheet.Dimension.Columns;
-
-        var range = sheet.Dimension.Address;
-
-        data = sheet
-            .Cells[range]
-            .ToCollection<T>(options =>
-            {
-                options.HeaderRow = hasTitle ? 0 : 1;
-                options.DataStartRow = hasTitle ? 1 : 0;
-                options.ConversionFailureStrategy =
-                    ToCollectionConversionFailureStrategy.SetDefaultValue;
-            });
-        return data;
+        stream.Position = 0;
+        return stream.Query<T>(sheetName: sheetName, hasHeader: hasTitle).ToList();
     }
 }
